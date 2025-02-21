c9e04890f6756d116c81a3f87a7437e30043e725
Simone Orsi <simahawk@gmail.com>
2025-02-21 10:52:00 +0100

    jsonifier: make pre-commit happy

--- jsonifier/tests/test_get_parser.py
+++ jsonifier/tests/test_get_parser.py
@@ -3,7 +3,6 @@
 # Simone Orsi <simahawk@gmail.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from unittest import mock
 
 from odoo import tools
 from odoo.exceptions import UserError
